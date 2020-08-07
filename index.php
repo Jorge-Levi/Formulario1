cb8a786fe81411720113bdcc9c18d32642d82544
Jorge Levi <tapialugardo29@gmail.com>
2020-08-07 09:46:07 -0500

    50

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
 <?php
+$errores = '';
+$enviado = '';
+if(isset($_POST['submit'])){
+    if(isset($_POST['nombre']) && !empty($_POST['nombre'])){
+        $nombre = $_POST['nombre'];
+        $nombre = trim($nombre);
+        $nombre = filter_var($nombre,FILTER_SANITIZE_STRING);
+        $nombre = htmlspecialchars($nombre);
+    }else{
+        $errores .= 'Por favor, ingresa un nombre<br>';
+    }
+
+    if(isset($_POST['correo']) && !empty($_POST['correo'])){
+        $correo = $_POST['correo'];
+        $correo = trim($correo);
+        $correo = filter_var($correo,FILTER_SANITIZE_EMAIL);
+        if(filter_var($correo,FILTER_VALIDATE_EMAIL)){
+            $correo = htmlspecialchars($correo);
+        }else{
+            $errores .= 'Por favor, ingresa un correo valido<br>';
+        }
+    }else{
+        $errores .= 'Por favor, ingresa un correo<br>';
+    }
+}
 
 require('index.view.php');
--- index.view.php
+++ index.view.php
@@ -8,11 +8,29 @@
 </head>
 <body>
 	<main class="wrap">
-			<form class="formulario">
+			<form class="formulario" method="POST" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
 				<input type="text" name="nombre" class="form-control" id="nombre" placeholder="Nombre:" value="">
 				<input type="email" name="correo" class="form-control" id="correo" placeholder="Correo:" value="">
 
 				<textarea name="mensaje" class="form-control" id="mensaje" placeholder="Mensaje: "></textarea>
+
+				<?php if(!empty($errores)): ?>
+					<div class="alert error">
+						<?php echo $errores?>
+					</div>
+				<?php elseif($enviado): ?>
+					<div class="alert success">
+						<p>Formulario enviado correctamente</p>
+					</div>
+				<?php endif;?>
+
+				<!-- <div class="alert error">
+					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur
+				</div>
+
+				<div class="alert success">
+					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur
+				</div> -->
 
 				<input type="submit" name="submit" class="btn btn-primary" value="Enviar Correo">
 			</form>
